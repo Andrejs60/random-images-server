5da8aae6f9c27cffce65a9c5ca83c7436a3eacd0
Andrejs <ajancevskis@gmail.com>
2022-07-18 09:54:54 +0100

    first commit

---
+++ app/Http/Controllers/ImagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImagesController extends Controller
+{
+    public function ascendingOrder(){
+        // TODO: return an array of URLs for 4 images in ascending alphabetical file name order
+    }
+
+    public function descendingOrder(){
+        // TODO: return an array of URLs for 4 images in descending alphabetical file name order
+    }
+
+    public function randomOrder(){
+        // TODO: return an array of URLs for 4 images in random file name order
+    }
+}
